9b6ef8e829b0cdabd52079ee82872fa5bd66d0cd
Your Name <tonygiamboy@yahoo.com>
2018-05-20 20:11:05 -0400

    followed the video example

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const expHbs = require("express-handlebars");
+
+const app = express();
+const routes = require('./controllers/routes.js');
+app.use('/', routes);
+
+app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.urlencoded({
+    extended:false
+}));
+
+app.use(methodOverride('_method'));
+
+app.engine('handlebars', expHbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, function() {
+    console.log('Server listening on http://localhost: ' + PORT);
+});
